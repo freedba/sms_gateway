b31a4d821ad1b9f3b85036a3993fb2b80fbf973b
jason <xie.he@bingte.com>
2021-10-28 15:34:06 +0800

    更改

--- server/message.go
+++ server/message.go
@@ -66,7 +66,7 @@ func SubmitMsgIdToQueue(s *SrvConn) {
 					content = utils.GbkToUtf8(content)
 				}
 				hsm.TaskContent = string(content)
-				hsm.DevelopNo = string(p.SrcId.Data[len(s.Account.CmppDestId):])
+				hsm.DevelopNo = strings.TrimSpace(string(p.SrcId.Data[len(s.Account.CmppDestId):]))
 				//logger.Debug().Msgf("hsm.DevelopNo:%s",hsm.DevelopNo)
 				for _, v := range p.DestTerminalId {
 					destTerminalId = append(destTerminalId, v.String())
